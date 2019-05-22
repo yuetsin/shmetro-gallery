9451bded048507396723ed7635e9bda5ab16634b
yuetsin <akaza_akari@sjtu.edu.cn>
2019-05-22 10:02:22 +0800

    [add] complete station view localization

--- metro-gallery/metro-gallery/VC/StationDetailViewController.swift
+++ metro-gallery/metro-gallery/VC/StationDetailViewController.swift
@@ -7,55 +7,71 @@
 //
 
 import Cocoa
+import Localize_Swift
 import MapKit
 import SwiftyJSON
-import Localize_Swift
 
 class StationDetailViewController: NSViewController, L11nRefreshDelegate {
-
     @objc dynamic var stationName: String = "$STATION_NAME$"
     @objc dynamic var stationNameEn: String = "$STATION_NAME_EN$"
-    
+
     @objc dynamic var startTime: String = ""
     @objc dynamic var endTime: String = ""
     @objc dynamic var currentTime: String = ""
     @objc dynamic var isTomorrow: Bool = false
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    @IBOutlet weak var stripeA: NSTextField!
-    @IBOutlet weak var stripeB: NSTextField!
-    @IBOutlet weak var stripeC: NSTextField!
-    @IBOutlet weak var stripeD: NSTextField!
-    
-    
-    @IBOutlet weak var startOpText: NSTextField!
-    @IBOutlet weak var currentTimeText: NSTextField!
-    @IBOutlet weak var endOpText: NSTextField!
-    @IBOutlet weak var isNextDayText: NSTextField!
-    
-    @IBOutlet weak var lineAndDestSelector: NSPopUpButton!
-    
+
+    @IBOutlet var mapView: MKMapView!
+
+    @IBOutlet var stripeA: NSTextField!
+    @IBOutlet var stripeB: NSTextField!
+    @IBOutlet var stripeC: NSTextField!
+    @IBOutlet var stripeD: NSTextField!
+
+    @IBOutlet var startOpText: NSTextField!
+    @IBOutlet var endOpText: NSTextField!
+    @IBOutlet var isNextDayText: NSTextField!
+
+    @IBOutlet var lineAndDestSelector: NSPopUpButton!
+
+    @IBOutlet var generalTabItem: NSTabViewItem!
+    @IBOutlet var locationTabItem: NSTabViewItem!
+    @IBOutlet var timeTableBox: NSBox!
+    @IBOutlet var facilitiesBox: NSBox!
+    @IBOutlet var firstTrainLabel: NSTextField!
+    @IBOutlet var lastTrainLabel: NSTextField!
+    @IBOutlet var nextDayLabel: NSTextField!
+    @IBOutlet var toiletRadioButton: NSButton!
+    @IBOutlet var elevatorRadioButton: NSButton!
+
     @IBAction func capabilitiesSelected(_ sender: NSButton) {
-        
     }
-    
+
     func flushUILocalization() {
-        loadDetail()
+//        loadDetail()
+        renderTimeTable()
+        generalTabItem.label = genLocalizationString(zhHans: "一般", en: "General")
+        locationTabItem.label = genLocalizationString(zhHans: "位置", en: "Location")
+        timeTableBox.title = genLocalizationString(zhHans: "首末班车时间", en: "First and Last Train")
+        facilitiesBox.title = genLocalizationString(zhHans: "车站设施", en: "Station Facilities")
+        firstTrainLabel.stringValue = genLocalizationString(zhHans: "首班车", en: "First Train")
+        lastTrainLabel.stringValue = genLocalizationString(zhHans: "末班车", en: "Last Train")
+        nextDayLabel.stringValue = genLocalizationString(zhHans: "次日", en: "Next Day")
+        toiletRadioButton.title = genLocalizationString(zhHans: "卫生间", en: "Toilet")
+        elevatorRadioButton.title = genLocalizationString(zhHans: "无障碍电梯", en: "Elevator")
     }
-    
+
     @IBAction func timeTableOptionSelected(_ sender: NSPopUpButton) {
         if timetables.count == 0 {
             return
         }
-        
+
         let timePiece = timetables[timetableStrings.firstIndex(of: sender.titleOfSelectedItem ?? "") ?? 0]
-        
+
         startTime = (timePiece.dictionary?["first_time"]?.stringValue ?? "??:??")
             .replacingOccurrences(of: " ", with: "")
         endTime = (timePiece.dictionary?["last_time"]?.stringValue ?? "??:??")
             .replacingOccurrences(of: " ", with: "")
-        
+
         if startTime.replacingOccurrences(of: " ", with: "").count == 0 {
             if SuperManager.UILanguage == .chinese {
                 startTime = "未定"
@@ -63,7 +79,7 @@ class StationDetailViewController: NSViewController, L11nRefreshDelegate {
                 startTime = "Uncertain"
             }
         }
-        
+
         if endTime.replacingOccurrences(of: " ", with: "").count == 0 {
             if SuperManager.UILanguage == .chinese {
                 endTime = "未定"
@@ -71,94 +87,109 @@ class StationDetailViewController: NSViewController, L11nRefreshDelegate {
                 endTime = "Uncertain"
             }
         }
-        
-        
+
         isTomorrow = !endTime.starts(with: "0")
     }
-    
+
     var stations: [Station] = []
-    
+
     var timetables: [JSON] = []
-    
+
     var timetableStrings: [String] = []
-    
+
     var stationIdStr: String = ""
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        flushUILocalization()
     }
-    
+
     func loadDetail() {
-        initStationData(simpleStation: SimpleStation(StationKeyStr: stationIdStr,
-                                                     StationName: stationName),
-                        stationDetailCompletion: { station in
-                            if station != nil {
-                                self.stations.append(station!)
-                                self.flushUIElement(NSButton())
-                                self.loadTimeTable()
-                            }
-        })
+        if SuperManager.UILanguage == .chinese {
+            initStationData(simpleStation: SimpleStation(StationKeyStr: stationIdStr,
+                                                         StationName: stationName),
+                            stationDetailCompletion: { station in
+                                if station != nil {
+                                    self.stations.append(station!)
+                                    self.flushUIElement(NSButton())
+                                    self.loadTimeTable()
+                                }
+            })
+        } else {
+            initStationData(simpleStation: SimpleStation(StationKeyStr: stationIdStr,
+                                                         StationName: stationNameEn),
+                            stationDetailCompletion: { station in
+                                if station != nil {
+                                    self.stations.append(station!)
+                                    self.flushUIElement(NSButton())
+                                    self.loadTimeTable()
+                                }
+            })
+        }
     }
-    
+
     fileprivate func loadTimeTable() {
         if stations.count == 0 {
             return
         }
-        
+
         if timetables.count != 0 {
             return
         }
-        
+
         let stationCode = stations[0].stationCode
-        
+
         timetables = TimeTableManager.getTimeTableRelatedStation(stationCode)
-        
+
         renderTimeTable()
     }
-    
+
     fileprivate func renderTimeTable() {
         lineAndDestSelector.removeAllItems()
-        
+
         for timePiece in timetables {
             let itemTitle = generateSelectTime(timePiece["line"].intValue, timePiece["description"].stringValue)
             timetableStrings.append(itemTitle)
         }
-        
+
         if timetables.count == timetableStrings.count {
             lineAndDestSelector.addItems(withTitles: timetableStrings)
         } else {
             timetables.removeAll()
             timetableStrings.removeAll()
-            loadDetail()
+//            loadDetail()
         }
         timeTableOptionSelected(NSPopUpButton())
     }
-    
+
     @IBAction func flushUIElement(_ sender: NSButton) {
-        if (stations.count == 0) {
+        if stations.count == 0 {
             return
         }
-        
+
+        let coordinate = CLLocationCoordinate2D(latitude: (stations[0].stationPosition.0), longitude: (stations[0].stationPosition.1))
+
+        JZLocationConverter.default.bd09ToGcj02(coordinate, result: setAnnotation(_:))
+
+        drawLineColor()
+
+//        flushUILocalization()
+
         if SuperManager.UILanguage == .chinese {
+            stationName = stations[0].stationName
             stationNameEn = stations[0].stationNameEn
         } else {
-            stationNameEn = stationName
             stationName = stations[0].stationNameEn
+            stationNameEn = stations[0].stationName
         }
-        
-        let coordinate = CLLocationCoordinate2D(latitude: (stations[0].stationPosition.0), longitude: (stations[0].stationPosition.1))
-        
-        JZLocationConverter.default.bd09ToGcj02(coordinate, result: setAnnotation(_:))
-        
-        drawLineColor()
     }
-    
-    func setAnnotation(_ gcj2Point: CLLocationCoordinate2D) -> Void {
+
+    func setAnnotation(_ gcj2Point: CLLocationCoordinate2D) {
         let region = MKCoordinateRegion(center: gcj2Point,
                                         latitudinalMeters: CLLocationDistance(defaultScaleMeters),
                                         longitudinalMeters: CLLocationDistance(defaultScaleMeters))
-        
+
         mapView.showsScale = false
         mapView.showsCompass = true
         mapView.showsPointsOfInterest = true
@@ -169,39 +200,39 @@ class StationDetailViewController: NSViewController, L11nRefreshDelegate {
                           stations[0].stationName,
                           stations[0].stationNameEn))
     }
-    
+
     func drawLineColor() {
         let lines = stations[0].stationOfLinesId
         var colors: [NSColor] = []
         var textColors: [NSColor] = []
-        
+
         for metroLine in ViewController.metroLines {
             if lines.contains(metroLine.lineId) {
                 colors.append(getBorderColor(metroLine.primaryColor))
                 textColors.append(getLineTextColor(metroLine.lineId))
             }
         }
-        
+
         let colorsCount = colors.count
-        
+
         stripeA.backgroundColor = .clear
         stripeB.backgroundColor = .clear
         stripeC.backgroundColor = .clear
         stripeD.backgroundColor = .clear
-        
+
         if colorsCount > 0 {
             stripeA.backgroundColor = colors[0]
             if colorsCount > 1 {
-                 stripeB.backgroundColor = colors[1]
+                stripeB.backgroundColor = colors[1]
                 if colorsCount > 2 {
-                     stripeC.backgroundColor = colors[2]
+                    stripeC.backgroundColor = colors[2]
                     if colorsCount > 3 {
-                         stripeD.backgroundColor = colors[3]
+                        stripeD.backgroundColor = colors[3]
                     }
                 }
             }
         }
-        
+
         if SuperManager.acessibilityMode {
             if colorsCount > 0 {
                 stripeA.stringValue = getMinorIconText(lines[0])
